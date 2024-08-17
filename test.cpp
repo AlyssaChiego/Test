df8336f9065bd9f1edd5a320085d78900149ff9f
Alyssa Chiego <abmccurdy7151@eagle.fgcu.edu>
2024-08-17 14:13:24 -0400

    redo of cmake file for SFML

--- test.cpp
+++ test.cpp
@@ -1,3 +1,27 @@
-//
-// Created by Owner on 8/11/2024.
-//
+#include <iostream>
+#include <SFML/Window.hpp>
+
+int main(){
+
+    sf::Window window(sf::VideoMode(10, 10), "Window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+//    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+//    {
+//        std::cout << "A";
+//    }
+
+    return 0;
+}
